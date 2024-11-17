a45e6f83b9c069445b4eda5b9a8aa57da3626f3d
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-17 13:17:02 +0100

    Start implement form logic

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,21 @@
 function Form() {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What o you need for your ✈️ trip?</h3>
-        </div>
+            <select>
+                {Array.from({length: 20}, (_, i) => i + 1).map(num =>
+                    <option value={num} key={num}>
+                        {num}
+                    </option>)}
+            </select>
+            <input type="text" placeholder="Item..." />
+            <button>Add</button>
+        </form>
     )
 }
 
