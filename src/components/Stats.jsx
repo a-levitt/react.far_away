a987cc4b65bf346b672e6a61e445baae95d3dbd0
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-23 13:27:13 +0100

    Implement stats component logics

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,25 @@
-function Stats() {
+function Stats({items}) {
+    if (!items.length) {
+        return (
+            <p className="stats">
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        )
+    }
+
+    const numItems = items.length;
+    const numPacked = items.filter(item => item.packed).length;
+    const percentagePacked = Math.round(numPacked/numItems * 100)
+
     return (
         <footer className="stats">
-            💼 <em> You have X items on your list, and you already packed Y (Y%) </em>
-        </footer>
-    )
-}
+            💼 <em>
+            {percentagePacked === 100 ?
+                'You got everything! You are ready to go! ✈️' :
+                `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentagePacked}%)` }
+            </em>
+        < /footer>
+            )
+        }
 
-export default Stats;
+            export default Stats;
